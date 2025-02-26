7b6a08b11ff00e8c83450a9da9b5fb4faeaef599
Esteban Canteros <estebanacanteros@gmail.com>
2025-02-26 10:49:35 -0300

    add authGuard to validate auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guards/auth.guard';
+import { User } from './decorators/user.decorator';
+import { RequestUser } from './interfaces/request-user.interface';
+import { Token } from './decorators/token.decorator';
 
 @Controller('/auth')
 export class AuthController {
@@ -27,12 +39,9 @@ export class AuthController {
     );
   }
 
+  @UseGuards(AuthGuard)
   @Get('/verify')
-  verifyUser() {
-    return this.client.send('auth.verify.user', {}).pipe(
-      catchError((err) => {
-        throw new RpcException(err);
-      }),
-    );
+  verifyUser(@User() user: RequestUser, @Token() token: string) {
+    return { user, token };
   }
 }
---
+++ src/auth/interfaces/request-user.interface.ts
@@ -0,0 +1,5 @@
+export class RequestUser {
+  name: string;
+  email: string;
+  id: string;
+}
